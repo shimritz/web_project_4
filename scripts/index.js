23dd6d82f535e45a2ee74b079d68ce3a0fffa661
Shimrit breef Ziskand <shimrit021@gmail.com>
2022-03-31 13:56:57 +0100

    fix: reviwer comments fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,14 @@
 const openModalButton = document.querySelector(".profile__edit-button");
 const closeModalButton = document.querySelector(".modal__close-btn");
-const saveModalButton = document.querySelector(".modal__form-submit");
 const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
+const modal__container = document.querySelector(".modal__container");
+
 
 const profileName = document.querySelector(".profile__name");
-const profileAboutMe = document.querySelector(".profile__aboutMe");
+const profileAboutMe = document.querySelector(".profile__about-me");
 
 //forms
 const profileForm = document.forms.profile;
-// const profileForm = document.querySelector(".profile");
 const profileFormNameInput = profileForm.elements.name;
 const profileFormAboutMeInput = profileForm.elements.aboutMe;
 
@@ -17,22 +16,22 @@ const profileFormAboutMeInput = profileForm.elements.aboutMe;
 function handleProfileFormSubmit() {
     profileFormNameInput.value = profileName.textContent;
     profileFormAboutMeInput.value = profileAboutMe.textContent;
-     modal.style.display = "flex";
-    overlay.style.display = "block";
+    modal__container.classList.add("modal__container-open");
+    modal.classList.add("modal__open");
 }
 
 profileForm.addEventListener('submit', function (event) {
     profileName.textContent = profileFormNameInput.value;
     profileAboutMe.textContent = profileFormAboutMeInput.value;
-    modal.style.display = "none";
-    overlay.style.display = "none";
+        modal__container.classList.remove("modal__container-open");
+    modal.classList.remove("modal__open");
     event.preventDefault();
 })
 
 openModalButton.addEventListener('click', handleProfileFormSubmit);
 
-
-closeModalButton.addEventListener('click', function (event) {
-    modal.style.display = "none";
-    overlay.style.display = "none";
-});
+function closeModal() {
+    modal__container.classList.remove("modal__container-open");
+     modal.classList.remove("modal__open");
+}
+closeModalButton.addEventListener('click', closeModal)
