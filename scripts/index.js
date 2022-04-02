30aa01b23d1d8a2b78cd7c5b57822adaefae6457
Shimrit breef Ziskand <shimrit021@gmail.com>
2022-04-02 21:01:24 +0100

    fix: seconde itteration reviwer fixes added

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const openModalButton = document.querySelector(".profile__edit-button");
 const closeModalButton = document.querySelector(".modal__close-btn");
 const modal = document.querySelector(".modal");
-const modal__container = document.querySelector(".modal__container");
+const modalContainer = document.querySelector(".modal__container");
 
 
 const profileName = document.querySelector(".profile__name");
@@ -16,22 +16,19 @@ const profileFormAboutMeInput = profileForm.elements.aboutMe;
 function handleProfileFormSubmit() {
     profileFormNameInput.value = profileName.textContent;
     profileFormAboutMeInput.value = profileAboutMe.textContent;
-    modal__container.classList.add("modal__container-open");
-    modal.classList.add("modal__open");
+    modal.classList.add("modal_open");
+}
+
+function closeModal() {
+     modal.classList.remove("modal_open");
 }
 
 profileForm.addEventListener('submit', function (event) {
     profileName.textContent = profileFormNameInput.value;
     profileAboutMe.textContent = profileFormAboutMeInput.value;
-        modal__container.classList.remove("modal__container-open");
-    modal.classList.remove("modal__open");
+    closeModal();
     event.preventDefault();
 })
 
 openModalButton.addEventListener('click', handleProfileFormSubmit);
-
-function closeModal() {
-    modal__container.classList.remove("modal__container-open");
-     modal.classList.remove("modal__open");
-}
 closeModalButton.addEventListener('click', closeModal)
