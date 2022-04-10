13b63059281f90a6f8ef842aaee0cb72f4686701
Shimrit breef Ziskand <shimrit021@gmail.com>
2022-04-10 14:10:54 +0100

    feat: adding cards to page with js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg"
+  }
+];
+
 const openModalButton = document.querySelector(".profile__edit-button");
 const closeModalButton = document.querySelector(".modal__close-btn");
 const modal = document.querySelector(".modal");
@@ -12,7 +39,10 @@ const profileForm = document.forms.profile;
 const profileFormNameInput = profileForm.elements.name;
 const profileFormAboutMeInput = profileForm.elements.aboutMe;
 
+//wrappers
+const cardsList = document.querySelector(".photos");
 
+// function createCardElement()
 function handleProfileFormSubmit() {
     profileFormNameInput.value = profileName.textContent;
     profileFormAboutMeInput.value = profileAboutMe.textContent;
@@ -32,3 +62,16 @@ profileForm.addEventListener('submit', function (event) {
 
 openModalButton.addEventListener('click', handleProfileFormSubmit);
 closeModalButton.addEventListener('click', closeModal)
+
+const cardTemplate = document.querySelector("#card-template").content.querySelector(".card");
+initialCards.forEach(card => {
+    const cardElement = cardTemplate.cloneNode(true);
+    const cardImage = cardElement.querySelector(".card__image");
+    const cardTitle = cardElement.querySelector(".card__name");
+
+
+    cardImage.src = card.link;
+    cardTitle.textContent = card.name;
+
+    cardsList.appendChild(cardElement);
+})
